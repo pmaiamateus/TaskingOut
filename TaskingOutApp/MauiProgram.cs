ea81c2f188ffe2830f1d06fd97277c2637d9a9f6
mateus.maia <mateus.maia@sistemasdlink.com.br>
2023-12-29 16:30:12 -0300

    upgrade:  installing mublazor templates and blazored localstorage packages

--- TaskingOutApp/MauiProgram.cs
+++ TaskingOutApp/MauiProgram.cs
@@ -1,30 +1,31 @@
 ﻿using Microsoft.Extensions.Logging;
 using TaskingOutApp.Data;
+using MudBlazor.Services;
 
-namespace TaskingOutApp
+namespace TaskingOutApp;
+
+public static class MauiProgram
 {
-    public static class MauiProgram
+    public static MauiApp CreateMauiApp()
     {
-        public static MauiApp CreateMauiApp()
-        {
-            var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                });
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            });
 
-            builder.Services.AddMauiBlazorWebView();
+        builder.Services.AddMauiBlazorWebView();
+        builder.Services.AddMudServices();
 
 #if DEBUG
-    		builder.Services.AddBlazorWebViewDeveloperTools();
-    		builder.Logging.AddDebug();
+        builder.Services.AddBlazorWebViewDeveloperTools();
+		builder.Logging.AddDebug();
 #endif
 
-            builder.Services.AddSingleton<WeatherForecastService>();
+        builder.Services.AddSingleton<WeatherForecastService>();
 
-            return builder.Build();
-        }
+        return builder.Build();
     }
 }
