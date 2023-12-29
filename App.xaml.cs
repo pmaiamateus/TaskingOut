e049fdfa48443fe1f8085d2b798a1b7a0f79609a
mateus.maia <mateus.maia@sistemasdlink.com.br>
2023-12-29 15:44:08 -0300

    initial commit: creating app using visual studio base project

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Tasking_out
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Tasking_out
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
