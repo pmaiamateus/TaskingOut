d83463c4c50cf010160368ad7df129f9b2900495
mateus.maia <mateus.maia@sistemasdlink.com.br>
2023-12-29 23:19:48 -0300

    feat: creating models and main page for checklists

---
+++ TaskingOutApp/Data/Models/CheckTaskModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TaskingOutApp.Data.Models;
+
+public class CheckTask
+{
+    public int Id { get; set; }
+    public int Index { get; set; }
+    public string Description { get; set; } = "";
+    public bool IsChecked { get; set; }
+}
