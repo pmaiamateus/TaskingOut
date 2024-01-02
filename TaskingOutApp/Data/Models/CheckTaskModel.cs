d135b6496664fbc4cd73b3a60e9203c78401acde
mateus.maia <mateus.maia@sistemasdlink.com.br>
2024-01-02 20:42:10 -0300

    fix: fixing cards of checklists not showing the right content

--- TaskingOutApp/Data/Models/CheckTaskModel.cs
+++ TaskingOutApp/Data/Models/CheckTaskModel.cs
@@ -5,5 +5,5 @@ public class CheckTask
     public int Id { get; set; }
     public int Index { get; set; }
     public string Description { get; set; } = "";
-    public bool IsChecked { get; set; }
+    public bool IsChecked { get; set; } = false;
 }
